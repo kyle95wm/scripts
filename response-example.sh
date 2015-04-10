d4c7c6f4fc37b05075d7101ca718ebeda5af8a2b
Kyle Warwick-Mathieu <kyle95wm@users.noreply.github.com>
2015-04-09 21:53:18 -0400

    Create response-example.sh

---
+++ response-example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This is a sample script demonstrating if statements
+#If you answer is "y" then the "yay!" message will be printed
+#If your answer is "n" then the "you said no!" message will be printed.
+read -p "What is your answer? [y/n]" #Reads a user's response for y or n
+if [ $REPLY != "y" ] ; then #If the response is not "y" then print the message "you said no!"
+	echo "You said nn!" #Print this message if the user's response is not "y"
+else #If the user's response is "y" then......
+	echo "yay!" #Print this message is the response is "y"
+fi #Ends the if statement

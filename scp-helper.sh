2e778c63b22e1abf7d92a9ad6120dda7383ba278
kyle95wm <kyle95wm@gmail.com>
2016-11-01 16:56:56 -0400

    Getting started on scp script

--- scp-helper.sh
+++ scp-helper.sh
@@ -3,7 +3,6 @@ LOCALFILE="" # Grab the local file path
 USER="" # The username of the remote host
 HOST="" # The IP or FQDN of the remote host
 REMOTEDIR="" # Directory of the remote host - can be an absolute path to a file
-LOCALDIR="" # Directory of the local host - can be an absolute path to a file
 # The purpose of this script is to aid in using the rather complex SCP command
 
 # Menu
@@ -11,9 +10,19 @@ LOCALDIR="" # Directory of the local host - can be an absolute path to a file
 echo "1) Copy file from local host to remote host"
 echo "2) Copy file from remote host to local host"
 
-read -p "Please choose from the list by entering a number: "
+read -p "Please choose from the list by entering a number: " MENU
 
-# BEGIN COPY FROM LOCALHOST TO REMOTE HOST
-
-
-# WORK IN PROGRESS!
+if [ $MENU == "1" ] ; then
+	# BEGIN COPY FROM LOCALHOST TO REMOTE HOST
+	read -p "Please enter the FULL path of the file you would like copied to the remote server: " LOCALFILE
+	read -p "Please enter the username of the remote host: " USER
+	read -p "Please enter the host you would like to use (IP or FQDN): " HOST
+	read -p "Please enter a directory on the REMOTE host to copy $LOCALFILE to: " REMOTEDIR
+	echo "Copying $LOCALFILE as $USER to $HOST at $REMOTEDIR...."
+	scp "$LOCALFILE" $USER@$HOST:$REMOTEDIR
+	if $? != "0" ] ; then
+		echo "COPY FAILED!"
+		exit 1
+	fi
+	# END COPY FROM LOCALHOST TO REMOTE HOST
+fi

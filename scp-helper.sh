aafd55fd744521995f8a64452b04d23eaafa4c06
kyle95wm <kyle95wm@gmail.com>
2016-11-01 04:05:31 -0400

    Create SCP helper script

---
+++ scp-helper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOCALFILE="" # Grab the local file path
+USER="" # The username of the remote host
+HOST="" # The IP or FQDN of the remote host
+REMOTEDIR="" # Directory of the remote host - can be an absolute path to a file
+LOCALDIR="" # Directory of the local host - can be an absolute path to a file
+# The purpose of this script is to aid in using the rather complex SCP command
+
+# Menu
+
+echo "1) Copy file from local host to remote host"
+echo "2) Copy file from remote host to local host"
+
+read -p "Please choose from the list by entering a number: "
+
+# BEGIN COPY FROM LOCALHOST TO REMOTE HOST
+
+
+# WORK IN PROGRESS!

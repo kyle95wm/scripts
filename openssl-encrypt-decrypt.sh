955fb980d432b3e153ccd0deea151a247581dc40
kyle95wm <kyle95wm@gmail.com>
2018-04-28 20:11:30 -0400

    Improved file encryption script

--- openssl-encrypt-decrypt.sh
+++ openssl-encrypt-decrypt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo
-echo
+file=""
 
-echo "To encrypt a file:"
+if [ -z "$1" ] ; then
+	echo "Usage: $0 [file-to-encrypt-or-decrypt]"
+else
+	file="$1"
+fi
 
-echo "openssl enc -aes-256-cbc -e -in FILE-TO-ENCRYPT -out FINALFILE"
+echo Please\ pick\ what\ you\ would\ like\ to\ do\ with\ the\ file\ "$file".
+echo "1) Encrypt this file"
+echo "2) Decrypt this file"
+read -rp "Please enter 1 or 2: " opp
 
-echo
-echo "To decrypt a file:"
-
-echo "openssl enc -aes-256-cbc -d -in FILE-TO-DECRYPT -out FINALFILE"
+if [ "$opp" -eq "1" ] ; then
+	openssl enc -aes-256-cbc -e -in "$file" -out "$file"_encrypted
+	echo Your\ file\ has\ been\ encrypted\ as\ "$file"_encrypted.
+elif [ "$opp" -eq "2" ] ; then
+	openssl enc -aes-256-cbc -d -in "$file" -out "$file"_decrypted
+	echo Your\ file\ has\ been\ decrypted\ as\ "file"_decrypted.
+fi

c084215530fe20bc4121620f4cb7011bebda0557
kyle95wm <kyle95wm@gmail.com>
2019-01-05 06:53:20 -0500

    Shellcheck fixes

--- piholesync.rsync.sh
+++ piholesync.rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
+# shellcheck disable=SC2086
 #  README
 
 #Version 2.1
@@ -74,7 +74,7 @@ HAUSER=root #user of second pihole
 
 #LOOP FOR FILE TRANSFER
 RESTART=0 # flag determine if service restart is needed
-for FILE in ${FILES[@]}
+for FILE in "${FILES[@]}"
 do
   if [[ -f $PIHOLEDIR/$FILE ]]; then
   RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)

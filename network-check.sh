0910bc2ca673ac9bcdb41a7f4e4c91a769baad86
kyle95wm <kyle95wm@gmail.com>
2019-05-12 17:37:26 -0400

    Use full path for commands instead of commands themselves

--- network-check.sh
+++ network-check.sh
@@ -3,11 +3,11 @@
 # This script will check to see if the host can ping the router.
 # If the ping fails, shut down, then bring back up the interface designated as eth0
 
-if ! ping 192.168.101.100 -c4 ; then
+if ! /bin/ping 192.168.101.100 -c4 ; then
 	# We first shut down eth0
 	# After eth0 is shut down, we forcefully bring it back up
-	ifdown eth0 && ifup --force eth0
+	/sbin/ifdown eth0 && /sbin/ifup --force eth0
 else
 	# We do noting if the network is okay
-	echo "PBX is OK"
+	/bin/echo "PBX is OK"
 fi

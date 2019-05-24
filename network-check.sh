605fd1b29b78f16cd6a22d3a7cf0f51a78630150
kyle95wm <kyle95wm@gmail.com>
2019-05-24 17:31:47 -0400

    Changed some things

--- network-check.sh
+++ network-check.sh
@@ -3,10 +3,15 @@
 # This script will check to see if the host can ping the router.
 # If the ping fails, shut down, then bring back up the interface designated as eth0
 
+if [ $UID != 0 ] ; then
+	echo "Script must be run as root."
+	exit 1
+fi
+
 if ! /bin/ping 192.168.101.100 -c4 ; then
 	# We first shut down eth0
-	# After eth0 is shut down, we forcefully bring it back up
-	/sbin/ifdown eth0 && /sbin/ifup --force eth0
+	# After eth0 is shut down, we wait 5 seconds, then bring it back up
+	/sbin/ifconfig eth0 down && /bin/sleep 5 && /sbin/ifconfig eth0 up
 else
 	# We do noting if the network is okay
 	/bin/echo "PBX is OK"

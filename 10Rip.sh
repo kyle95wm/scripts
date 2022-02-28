466a5ca72aed7359921f24b045840ef40d01f742
BeanJr_YT <kyle95wm@gmail.com>
2022-02-28 00:10:44 -0500

    Improvements
    The script now uses the aria2c downloader and downloads 100 concurrent fragments. Downloads should take less than 2 minutes now.

--- 10Rip.sh
+++ 10Rip.sh
@@ -30,4 +30,4 @@ if [ -z "$yt" ] ; then
 	exit 1
 fi
 # The main command
-yt-dlp "$url" --username $user --password $pass --ffmpeg-location "$ff"
+yt-dlp "$url" --username $user --password $pass --ffmpeg-location "$ff" -concurrent-fragments 100 --downloader aria2c
